5f936782ce2a3805d4c94ffbaa98498dc8d7970c
DELL <DELL@DESKTOP-7OS91BQ>
2023-11-25 23:10:35 +0800

    Commiting Test2.java to BranchX

---
+++ src/com/mun/git/Test2.java
@@ -0,0 +1,11 @@
+package com.mun.git;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to chaitanyakumarg143 user");
+
+	}
+
+}
